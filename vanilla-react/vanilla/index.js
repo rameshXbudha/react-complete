d091850dd58ad04128ac8710e27755c7e171a2cd
Ramesh Budha <rameshxbudha@gmail.com>
2025-05-07 10:56:51 +0545

    Vanilla project

---
+++ vanilla-react/vanilla/index.js
@@ -0,0 +1,66 @@
+const content = [
+  [
+    "React is extremely popular",
+    "It makes building complex, interactive UIs a breeze",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecosystem",
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect())",
+    "Dynamic rendering",
+  ],
+  [
+    "Official web page (react.dev)",
+    "Next.js (Fullstack framework)",
+    "React Native (build native mobile apps with React)",
+  ],
+];
+
+const btnWhyReact = document.getElementById("btn-why-react");
+const btnCoreFeatures = document.getElementById("btn-core-features");
+const btnResources = document.getElementById("btn-resources");
+
+const tabContent = document.getElementById("tab-content");
+
+function displayContent(items) {
+  let listContent = "";
+  for (const item of items) {
+    listContent += `<li>${item}</li>`;
+  }
+
+  const list = document.createElement("ul");
+  tabContent.innerHTML = "";
+  list.innerHTML = listContent;
+  tabContent.append(list);
+}
+
+function highlightButton(btn) {
+  btnWhyReact.className = "";
+  btnCoreFeatures.className = "";
+  btnResources.className = "";
+
+  //
+  btn.className = "active";
+}
+
+function handleClick(event) {
+  const btnId = event.target.id;
+
+  highlightButton(event.target);
+
+  if (btnId === "btn-why-react") {
+    displayContent(content[0]);
+  } else if (btnId === "btn-core-features") {
+    displayContent(content[1]);
+  } else {
+    displayContent(content[2]);
+  }
+}
+
+displayContent(content[0]);
+
+btnWhyReact.addEventListener("click", handleClick);
+btnCoreFeatures.addEventListener("click", handleClick);
+btnResources.addEventListener("click", handleClick);
